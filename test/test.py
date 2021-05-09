195f83f441787ac25e9007451c5497601c0e1d5d
Srijarko Roy <66861243+srijarkoroy@users.noreply.github.com>
2021-05-09 15:44:36 +0530

    added test file

---
+++ test/test.py
@@ -0,0 +1,50 @@
+import cv2
+from tensorflow import keras
+
+model = keras.models.load_model('models/model.h5')
+
+labels_dict={0:'with_mask',1:'without_mask'}
+color_dict={0:(0,255,0),1:(0,0,255)}
+size = 4
+webcam=cv2.VideoCapture(0) #Use camera 0
+
+# We load the xml file
+classifier = cv2.CascadeClassifier('/media/adarshsrivastava/DATA/Face_mask/haarcascade_frontalface_default.xml')
+
+while True:
+    (rval, im) = webcam.read()
+    im=cv2.flip(im,1,1) #Flip to act as a mirror
+    
+    # Resize the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1]//size, im.shape[0]//size))
+    
+    # detect MultiScale / faces 
+    faces = classifier.detectMultiScale(mini)
+    
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+        #Save just the rectangle faces in SubRecFaces
+        face_img = im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(128,128))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,128,128,3))
+        reshaped = np.vstack([reshaped])
+        result=model.predict(reshaped)
+        label=np.argmax(result,axis=1)[0]
+
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+    # Show the image
+    cv2.imshow('LIVE',im)
+    key = cv2.waitKey(10)
+    # if Esc key is press then break out of the loop 
+    if key == 27: #The Esc key
+        break
+# Stop video
+webcam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
